4f43410437ab340bf8827daef1cbf1d9664adfb2
lukankerhubsources <126632323+lukankerhubsources@users.noreply.github.com>
2023-03-01 03:23:42 +0100

    Update Hub.lua

--- Hub.lua
+++ Hub.lua
@@ -1,4 +1,4 @@
-local Games = {
+local Hub = {
 	TreasureHuntSimulator = {
 		id = 1345139196,
 		source = 'https://raw.githubusercontent.com/lukankerhubsources/Treasure-Hunt-Simulator/main/Treasure%20Hunt%20Sim.lua'
@@ -12,3 +12,13 @@ local Games = {
 		source = 'https://raw.githubusercontent.com/lukankerhubsources/Touch-Football/main/Touch-Football.lua'
 	}
 }
+
+function Hub:GetLoadstring(GameID)
+	for i, Game in pairs(Hub) do
+		if GameID == Game.id then
+			return Game.source
+		end
+	end
+end
+
+return Hub
