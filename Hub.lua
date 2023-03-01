7b563c4b9207b8d1c44bbc6e10995816ed16e959
lukankerhubsources <126632323+lukankerhubsources@users.noreply.github.com>
2023-03-01 03:18:33 +0100

    Create Hub.lua

---
+++ Hub.lua
@@ -0,0 +1,14 @@
+local Games = {
+	TreasureHuntSimulator = {
+		id = 1345139196,
+		source = 'https://raw.githubusercontent.com/lukankerhubsources/Treasure-Hunt-Simulator/main/Treasure%20Hunt%20Sim.lua'
+	},
+	Deepwoken = {
+		id = 4111023553,
+		source = 'https://raw.githubusercontent.com/lukankerhubsources/Deepsploit/main/Loader.lua'
+	},
+	TouchFootball = {
+		id = 6125589657,
+		source = 'https://raw.githubusercontent.com/lukankerhubsources/Touch-Football/main/Touch-Football.lua'
+	}
+}
