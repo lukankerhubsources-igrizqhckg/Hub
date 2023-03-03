a7a9296e1cd59e91db907b42ecca3f1450610264
lukankerhubsources-igrizqhckg <126632323+lukankerhubsources-igrizqhckg@users.noreply.github.com>
2023-03-03 18:09:12 +0100

    Update Hub.lua

--- Hub.lua
+++ Hub.lua
@@ -4,15 +4,15 @@ local GitHubName = loadstring(game:HttpGet("https://pastebin.com/raw/sS94Uwjg"))
 local Games = {
 	TreasureHuntSimulator = {
 		id = 1345139196,
-		source = 'https://raw.githubusercontent.com/' + GitHubName + '/Treasure-Hunt-Simulator/main/Treasure%20Hunt%20Sim.lua'
+		source = 'https://raw.githubusercontent.com/' .. GitHubName .. '/Treasure-Hunt-Simulator/main/Treasure%20Hunt%20Sim.lua'
 	},
 	Deepwoken = {
 		id = 6032399813,
-		source = 'https://raw.githubusercontent.com/' + GitHubName + '/Deepsploit/main/Loader.lua'
+		source = 'https://raw.githubusercontent.com/' .. GitHubName .. '/Deepsploit/main/Loader.lua'
 	},
 	TouchFootball = {
 		id = 6125589657,
-		source = 'https://raw.githubusercontent.com/' + GitHubName + '/Touch-Football/main/Touch-Football.lua'
+		source = 'https://raw.githubusercontent.com/' .. GitHubName .. '/Touch-Football/main/Touch-Football.lua'
 	}
 }
 
