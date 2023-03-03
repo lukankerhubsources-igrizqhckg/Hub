ff11222dfa030edd03507370299f2adb01213657
lukankerhubsources <126632323+lukankerhubsources@users.noreply.github.com>
2023-03-03 18:02:31 +0100

    Update Hub.lua

--- Hub.lua
+++ Hub.lua
@@ -1,17 +1,18 @@
 local Hub = {}
+local GitHubName = loadstring(game:HttpGet("https://pastebin.com/raw/sS94Uwjg"))()
 
 local Games = {
 	TreasureHuntSimulator = {
 		id = 1345139196,
-		source = 'https://raw.githubusercontent.com/lukankerhubsources/Treasure-Hunt-Simulator/main/Treasure%20Hunt%20Sim.lua'
+		source = 'https://raw.githubusercontent.com/' + GitHubName + '/Treasure-Hunt-Simulator/main/Treasure%20Hunt%20Sim.lua'
 	},
 	Deepwoken = {
 		id = 6032399813,
-		source = 'https://raw.githubusercontent.com/lukankerhubsources/Deepsploit/main/Loader.lua'
+		source = 'https://raw.githubusercontent.com/' + GitHubName + '/Deepsploit/main/Loader.lua'
 	},
 	TouchFootball = {
 		id = 6125589657,
-		source = 'https://raw.githubusercontent.com/lukankerhubsources/Touch-Football/main/Touch-Football.lua'
+		source = 'https://raw.githubusercontent.com/' + GitHubName + '/Touch-Football/main/Touch-Football.lua'
 	}
 }
 
