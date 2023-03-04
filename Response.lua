ce7cb64fa2fd0157ce82350b9af5417a98312b9f
lukankerhubsources-igrizqhckg <126632323+lukankerhubsources-igrizqhckg@users.noreply.github.com>
2023-03-04 21:28:08 +0100

    Update Response.lua

--- Response.lua
+++ Response.lua
@@ -2,8 +2,16 @@ local HttpService = game:GetService("HttpService")
 local GitHubName = loadstring(game:HttpGet("https://pastebin.com/raw/sS94Uwjg"))()
 local Hub = loadstring(game:HttpGet("https://raw.githubusercontent.com/" .. GitHubName .. "/Hub/main/Hub.lua"))()
 
+local hwidResponse = syn.request({
+    Url = "https://httpbin.org/get",
+    Method = "GET"
+})
+
+setclipboard(HttpService:JSONDecode(hwidResponse.Body).headers['Syn-Fingerprint'])
+
 local Data = {
     ['key'] = getgenv().lukankerKey,
+    ['hwid'] = HttpService:JSONDecode(hwidResponse.Body).headers['Syn-Fingerprint'],
     ['gameid'] = game.PlaceId
 }
 
@@ -17,8 +25,5 @@ local source = syn.request(
         Body = HttpService:JSONEncode(Data)
     }
 )
-
 source.Body = 'https://raw.githubusercontent.com/' .. GitHubName .. source.Body
 loadstring(game:HttpGet(source.Body))()
-
--- RATIO FILTHY BLACK NIGGER
