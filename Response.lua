06112dcdc26610821690c6d51ee2c7dc396f7916
lukankerhubsources-igrizqhckg <126632323+lukankerhubsources-igrizqhckg@users.noreply.github.com>
2023-03-04 19:59:49 +0100

    Update Response.lua

--- Response.lua
+++ Response.lua
@@ -2,31 +2,8 @@ local HttpService = game:GetService("HttpService")
 local GitHubName = loadstring(game:HttpGet("https://pastebin.com/raw/sS94Uwjg"))()
 local Hub = loadstring(game:HttpGet("https://raw.githubusercontent.com/" .. GitHubName .. "/Hub/main/Hub.lua"))()
 
-local keyResponse = syn.request(
-    {
-        Url = "https://lukankerhub.com/encryption-igrizqhckg.php",  -- This website helps debug HTTP requests
-        Method = "POST",
-        Headers = {
-            ["Content-Type"] = "application/json"  -- When sending JSON, set this!
-        },
-        Body = HttpService:JSONEncode(getgenv().lukankerKey)
-    }
-)
-
-local user = keyResponse.Body
-
-local hwidResponse = syn.request({
-    Url = "https://httpbin.org/get",
-    Method = "GET"
-})
-
-local ip = game:HttpGet('https://api.ipify.org/?format=raw', true)
-local hwid = HttpService:JSONDecode(hwidResponse.Body).headers['Syn-Fingerprint']
-
 local Data = {
-    ['username'] = user,
-    ['ip'] = ip,
-    ['hwid'] = hwid,
+    ['key'] = getgenv().lukankerKey,
     ['gameid'] = game.PlaceId
 }
 
