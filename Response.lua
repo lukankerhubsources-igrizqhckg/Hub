16652fc7606c78a5d49211ca34d28cdf9c3dd96b
lukankerhubsources-igrizqhckg <126632323+lukankerhubsources-igrizqhckg@users.noreply.github.com>
2023-07-02 16:18:02 +0200

    Update Response.lua

--- Response.lua
+++ Response.lua
@@ -1,14 +1,15 @@
 local HttpService = game:GetService("HttpService")
 local GitHubName = loadstring(game:HttpGet("https://pastebin.com/raw/sS94Uwjg"))()
 
-local hwidResponse = syn.request({
-    Url = "https://httpbin.org/get",
-    Method = "GET"
-})
+-- local hwidResponse = syn.request({
+--     Url = "https://httpbin.org/get",
+--     Method = "GET"
+-- })
 
 local Data = {
     ['key'] = getgenv().lukankerKey,
-    ['hwid'] = HttpService:JSONDecode(hwidResponse.Body).headers['Syn-Fingerprint'],
+    --['hwid'] = HttpService:JSONDecode(hwidResponse.Body).headers['Syn-Fingerprint'],
+    ['hwid'] = game:GetService("RbxAnalyticsService"):GetClientId(),
     ['gameid'] = game.PlaceId
 }
 
